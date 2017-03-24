79ba1d211f00ae3808298cc2d2698d2311e4faee
pengfengwang <pengfengwang@sohu-inc.com>
2017-03-24 08:41:19 +0800

    sdk ver 判断
    Signed-off-by: pengfengwang <pengfengwang@sohu-inc.com>

--- skin-app/src/main/java/com/ximsfei/skindemo/tab/LastFragment.java
+++ skin-app/src/main/java/com/ximsfei/skindemo/tab/LastFragment.java
@@ -1,5 +1,6 @@
 package com.ximsfei.skindemo.tab;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -23,10 +24,12 @@ public class LastFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment, null);
         mEdit = (EditText) view.findViewById(R.id.edit);
-        mEdit.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.drawable_left_selector,
-                R.drawable.drawable_top_selector,
-                R.drawable.drawable_right_selector,
-                R.drawable.drawable_bottom_selector);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            mEdit.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.drawable_left_selector,
+                    R.drawable.drawable_top_selector,
+                    R.drawable.drawable_right_selector,
+                    R.drawable.drawable_bottom_selector);
+        }
         mEdit1 = (EditText) view.findViewById(R.id.edit1);
         mEdit1.setCompoundDrawablesWithIntrinsicBounds(R.drawable.drawable_left_selector,
                 R.drawable.drawable_top_selector,
