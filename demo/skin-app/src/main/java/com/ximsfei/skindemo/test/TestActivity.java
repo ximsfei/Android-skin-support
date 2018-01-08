8108ea5c52e146fb21197d5452e9021e06f5a66b
pengfengwang <550616352@qq.com>
2018-01-08 14:32:28 +0800

    issue #112 demo

--- demo/skin-app/src/main/java/com/ximsfei/skindemo/test/TestActivity.java
+++ demo/skin-app/src/main/java/com/ximsfei/skindemo/test/TestActivity.java
@@ -2,9 +2,12 @@ package com.ximsfei.skindemo.test;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.ViewStub;
 
 import com.ximsfei.skindemo.BaseActivity;
 import com.ximsfei.skindemo.R;
+import com.ximsfei.skindemo.mdtab.MDFirstFragment;
 
 public class TestActivity extends BaseActivity {
     @Override
@@ -12,5 +15,10 @@ public class TestActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
         initToolbar();
+        ViewStub viewStub = (ViewStub) findViewById(R.id.view_stub);
+        viewStub.setLayoutResource(R.layout.fragment_view_stub);
+        viewStub.inflate();
+        MDFirstFragment fragment = (MDFirstFragment) getSupportFragmentManager().findFragmentById(R.id.md_fragment);
+        Log.e("TestActivity", "fragment = " + fragment);
     }
 }
