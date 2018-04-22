99aa767f99bf54b7ad67983dc712b5a5a868953b
pengfeng wang <550616352@qq.com>
2018-04-22 22:42:52 +0800

    init skin-demo

---
+++ demo/skin-demo/src/main/java/com/ximsfei/skindemo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ximsfei.skindemo;
+
+import android.os.Bundle;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initBottomMenu();
+    }
+}
