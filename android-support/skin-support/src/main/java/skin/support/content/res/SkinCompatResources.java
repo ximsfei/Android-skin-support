261f244999779295b19a73626e8d95d1b5ab1adf
wgyscsf <wgyscsf@163.com>
2019-01-17 12:16:54 +0800

    开放SkinCompatResources#getTargetResId(Context context, int resId),允许用户直接获取对应资源的资源id

--- android-support/skin-support/src/main/java/skin/support/content/res/SkinCompatResources.java
+++ android-support/skin-support/src/main/java/skin/support/content/res/SkinCompatResources.java
@@ -105,7 +105,7 @@ public class SkinCompatResources {
         return getColorStateList(SkinCompatManager.getInstance().getContext(), resId);
     }
 
-    int getTargetResId(Context context, int resId) {
+    public int getTargetResId(Context context, int resId) {
         try {
             String resName = null;
             if (mStrategy != null) {
