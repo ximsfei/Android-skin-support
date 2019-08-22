f495dd8c221832982ebdb6db0ab2ea93689039c0
pengfeng wang <550616352@qq.com>
2019-08-23 00:18:13 +0800

    skin-androidx-app demo

--- demo/skin-sample/src/main/java/com/ximsfei/skin/sample/activities/components/ComponentActivity.java
+++
@@ -1,7 +0,0 @@
-package com.ximsfei.skin.sample.activities.components;
-
-import com.ximsfei.skin.sample.base.BaseActivity;
-
-public abstract class ComponentActivity extends BaseActivity {
-    public static final String KEY_TITLE = "title";
-}
