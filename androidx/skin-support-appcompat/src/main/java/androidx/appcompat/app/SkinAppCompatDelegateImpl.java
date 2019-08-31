0c026ded439f913525d8584e09e8cb43fcc69d42
pengfeng wang <550616352@qq.com>
2019-08-31 23:38:27 +0800

    fix memory leak

--- androidx/skin-support-appcompat/src/main/java/androidx/appcompat/app/SkinAppCompatDelegateImpl.java
+++ androidx/skin-support-appcompat/src/main/java/androidx/appcompat/app/SkinAppCompatDelegateImpl.java
@@ -4,17 +4,19 @@ import android.app.Activity;
 import android.content.Context;
 import android.view.Window;
 
+import java.lang.ref.WeakReference;
 import java.util.Map;
 import java.util.WeakHashMap;
 
 public class SkinAppCompatDelegateImpl extends AppCompatDelegateImpl {
-    private static Map<Activity, AppCompatDelegate> sDelegateMap = new WeakHashMap<>();
+    private static Map<Activity, WeakReference<AppCompatDelegate>> sDelegateMap = new WeakHashMap<>();
 
     public static AppCompatDelegate get(Activity activity, AppCompatCallback callback) {
-        AppCompatDelegate delegate = sDelegateMap.get(activity);
+        WeakReference<AppCompatDelegate> delegateRef = sDelegateMap.get(activity);
+        AppCompatDelegate delegate = (delegateRef == null ? null : delegateRef.get());
         if (delegate == null) {
             delegate = new SkinAppCompatDelegateImpl(activity, activity.getWindow(), callback);
-            sDelegateMap.put(activity, delegate);
+            sDelegateMap.put(activity, new WeakReference<>(delegate));
         }
         return delegate;
     }
