4ba9c983535ec25a91a8e5dfc5c4ab24699e3b82
Pengfeng Wang <ximsfei@gmail.com>
2020-05-25 23:37:05 +0800

    feat: skin-mobile demo

---
+++ demo/skin-mobile/src/main/java/skin/support/mobile/demo/MainActivity.java
@@ -0,0 +1,14 @@
+package skin.support.mobile.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
