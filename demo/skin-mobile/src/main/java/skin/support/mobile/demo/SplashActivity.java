c130b5f6471aa8e1f926123a8fe2430ee86c91cb
Pengfeng Wang <ximsfei@gmail.com>
2020-05-26 00:57:16 +0800

    feat: add SplashActivity

---
+++ demo/skin-mobile/src/main/java/skin/support/mobile/demo/SplashActivity.java
@@ -0,0 +1,26 @@
+package skin.support.mobile.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        TextView buildTimeTv = findViewById(R.id.tv_build_time);
+        buildTimeTv.setText(String.format(getString(R.string.splash_build_time), BuildConfig.BUILD_TIME));
+        buildTimeTv.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
