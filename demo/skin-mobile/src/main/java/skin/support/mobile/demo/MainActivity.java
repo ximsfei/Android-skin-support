20baad9ec3fe7a9e0fd6d3f6cdb9f6744dec410d
Pengfeng Wang <ximsfei@gmail.com>
2020-05-26 22:06:27 +0800

    feat: main activity

--- demo/skin-mobile/src/main/java/skin/support/mobile/demo/MainActivity.java
+++ demo/skin-mobile/src/main/java/skin/support/mobile/demo/MainActivity.java
@@ -1,14 +1,75 @@
 package skin.support.mobile.demo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
+
+import skin.support.mobile.demo.fragment.base.BaseFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView mMenuBnv;
+    private ViewPager2 mContainerVp;
+    private ArrayList<Fragment> mFragments;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mMenuBnv = findViewById(R.id.bnv_menu);
+        mContainerVp = findViewById(R.id.vp_container);
+        initViews();
+    }
+
+    private void initViews() {
+        mFragments = new ArrayList<>();
+        mFragments.add(new BaseFragment());
+        mFragments.add(new BaseFragment());
+        mFragments.add(new BaseFragment());
+        mContainerVp.setAdapter(new FragmentStateAdapter(this) {
+            @NonNull
+            @Override
+            public Fragment createFragment(int position) {
+                return mFragments.get(position);
+            }
+
+            @Override
+            public int getItemCount() {
+                return mFragments.size();
+            }
+        });
+        mContainerVp.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                mMenuBnv.getMenu().getItem(position).setChecked(true);
+            }
+        });
+        mMenuBnv.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.bnv_home:
+                        mContainerVp.setCurrentItem(0);
+                        return true;
+                    case R.id.bnv_discovery:
+                        mContainerVp.setCurrentItem(1);
+                        return true;
+                    case R.id.bnv_mine:
+                        mContainerVp.setCurrentItem(2);
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
---
+++ demo/skin-mobile/src/main/java/skin/support/mobile/demo/fragment/base/BaseFragment.java
@@ -0,0 +1,20 @@
+package skin.support.mobile.demo.fragment.base;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import skin.support.mobile.demo.R;
+
+public class BaseFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_base, container, false);
+    }
+}
